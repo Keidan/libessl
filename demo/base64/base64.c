d6e66bbc239c731be265b2cc7cbd805fb43bf485
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2016-04-21 17:30:49 +0200

    Fix GCC warns

--- demo/base64/base64.c
+++ demo/base64/base64.c
@@ -2,6 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GCC_VERSION (__GNUC__ * 10000 \
+                               + __GNUC_MINOR__ * 100 \
+                               + __GNUC_PATCHLEVEL__)
+#if GCC_VERSION > 40900 /* 4.9.0 */
+  #define PRINTF_SIZE_T "zu"
+#else
+  #define PRINTF_SIZE_T "lu"
+#endif
+
+
 int main(int argc, char** argv) {
 
   const char* str = "Hello world";
@@ -13,12 +23,12 @@ int main(int argc, char** argv) {
   (void)argv;/* remove warning */
 
   if(essl_base64_encode(str, strlen(str), &output, &olength) == 0)
-    printf("%s: '%s' (%lu)\n", str, output, olength);
+    printf("%s: '%s' (%"PRINTF_SIZE_T")\n", str, output, olength);
   else fprintf(stderr, "Unable to base64 encode: (%d) %s\n", errno, strerror(errno));
   
   
   if(essl_base64_decode(output, olength, &output2, &olength) == 0)
-    printf("%s: '%s' (%lu)\n", output, output2, olength);
+    printf("%s: '%s' (%"PRINTF_SIZE_T")\n", output, output2, olength);
   else fprintf(stderr, "Unable to base64 decode: (%d) %s\n", errno, strerror(errno));
 
   free(output2);
