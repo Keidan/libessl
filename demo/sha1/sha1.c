5904eda0f953277be11b636b91cd1d742fe7fa4b
Kevin BILLONNEAU <kuso.fr@gmail.com>
2016-06-29 08:40:54 +0200

    add presence test for the SHA support

--- demo/sha1/sha1.c
+++ demo/sha1/sha1.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-
+#ifndef OPENSSL_NO_SHA
   const char* str = "Hello world";
   essl_sha1_string_t output;
 
@@ -16,6 +16,8 @@ int main(int argc, char** argv) {
   if(essl_sha1_do_hash_file(argv[0], output) == 0)
     printf("%s: '%s'\n", argv[0], output);
   else fprintf(stderr, "Unable to sha1: (%d) %s\n", errno, strerror(errno));
-
+#else
+  printf("SHA not supported\n");
+#endif /* OPENSSL_NO_SHA */
   return 0;
 }
